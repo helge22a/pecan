6decd683b638f447f294812bf82aa847e1cf40cf
Alexis Helgeson <ahelgeso@bu.edu>
2023-02-15 11:27:53 -0500

    "updated download fcn"

--- scripts/EFI_dataprep.R
+++ scripts/EFI_dataprep.R
@@ -23,22 +23,50 @@ start_date = format(Sys.Date()-1, "%Y-%m-%d")
 for(i in 1:length(sitename)){
 
 ###### Download Data ###########
-
-download_noaa_files_s3 <- function(siteID, date, cycle, local_directory){
+#depreciated fcn
+# download_noaa_files_s3 <- function(siteID, date, cycle, local_directory){
+#   
+#   Sys.setenv("AWS_DEFAULT_REGION" = "data",
+#              "AWS_S3_ENDPOINT" = "ecoforecast.org")
+#   
+#   object <- aws.s3::get_bucket("drivers", prefix=paste0("noaa/NOAAGEFS_1hr/",siteID,"/",date,"/",cycle))
+#   
+#   for(j in 1:length(object)){
+#     aws.s3::save_object(object[[j]], bucket = "drivers", file = file.path(local_directory, object[[j]]$Key))
+#   }
+# }
+#new fcn taken from eco4cast/neon4cast/s3_helpers.R https://github.com/eco4cast/neon4cast/blob/main/R/s3_helpers.R
+download_s3_objects <- function(dir, bucket, prefix, s3_region = Sys.getenv("AWS_DEFAULT_REGION")){
   
-  Sys.setenv("AWS_DEFAULT_REGION" = "data",
-             "AWS_S3_ENDPOINT" = "ecoforecast.org")
-  
-  object <- aws.s3::get_bucket("drivers", prefix=paste0("noaa/NOAAGEFS_1hr/",siteID,"/",date,"/",cycle))
-  
-  for(j in 1:length(object)){
-    aws.s3::save_object(object[[j]], bucket = "drivers", file = file.path(local_directory, object[[j]]$Key))
+  files <- aws.s3::get_bucket(bucket = bucket,
+                              prefix = prefix,
+                              region = s3_region,
+                              use_https = as.logical(Sys.getenv("USE_HTTPS")),
+                              max = Inf)
+  keys <- vapply(files, `[[`, "", "Key", USE.NAMES = FALSE)
+  empty <- grepl("/$", keys)
+  keys <- keys[!empty]
+  files_present <- TRUE
+  if(length(keys) > 0){
+    for(i in 1:length(keys)){
+      aws.s3::save_object(object = keys[i],
+                          bucket = bucket,
+                          file = file.path(dir, bucket, keys[i]),
+                          region = s3_region,
+                          use_https = as.logical(Sys.getenv("USE_HTTPS")))
+    }
+  }else{
+    message("Requested files are not available on the s3 bucket")
+    files_present <- FALSE
   }
+  invisible(files_present)
 }
 
+# download_noaa_files_s3(siteID = sitename[i], date = as.Date(start_date), cycle = "00", local_directory <- base_dir[i] )# }
 
-download_noaa_files_s3(siteID = sitename[i], date = as.Date(start_date), cycle = "00", local_directory <- base_dir[i] )# }
-
+download_s3_objects(dir = base_dir[i], bucket = "drivers", 
+                    prefix = paste0("noaa/NOAAGEFS_1hr/",sitename[i],"/",as.Date(start_date),"/","00"),
+                    s3_region = Sys.getenv("AWS_DEFAULT_REGION"))
 
 ############ Downscale to 30 minutes ##############
 
